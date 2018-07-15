ac3cd2413d1d4d918265dd494ac47ddf6176f7ec
Nilesh <agnilesh8199@gmail.com>
2018-07-16 01:48:42 +0530

    Instructions commit

--- Stack/stack1.py
+++ Stack/stack1.py
@@ -24,7 +24,9 @@ push(stack, str(10))
 push(stack, str(20))
 push(stack, str(30))
 push(stack, str(40))
+# add your insertions here the same way as above
 print("Before popping up the value : " + str(stack))
+# pop the values multiple times using the same syntax as above
 print(pop(stack) + " popped from stack")
 print("After popping up the stack looks : " + str(stack))
 print("Top item is " + str(stack[-1:]))
